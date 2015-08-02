6b454d96a809cb08e251550a86f1f0a8503cf1d2
Maradona Morais <maradona.morais@hotmail.com>
2015-08-02 10:27:19 -0300

    Criação do modelo de Gerente

---
+++ application/models/Gerente_md.php
@@ -0,0 +1,31 @@
+<?php
+class Gerente_md extends CI_Model {
+	public $id;
+	public $nome;
+	public $sobrenome;
+	public $email;
+	public $tipo;
+	
+	public function __construct() {
+		// Call the CI_Model constructor
+		parent::__construct();
+	}
+	
+	public function cadastrarGerente($nome, $sobrenome, $email, $senha) {
+		$this->load->database();
+		$dados = array("nome"=>$nome, "sobrenome"=>$sobrenome, "email"=>$email, "senha"=>$senha, "tipo"=>1);
+		$this->db->insert('gerente', $dados);
+		
+		return $this->db->insert_id();
+	}
+	
+	public function emailExiste($email) {
+		$this->load->database();
+		$sql = "SELECT * FROM gerente WHERE email = ?";
+		$resultado = $this->db->query($sql, array($email));
+		if ($resultado->num_rows() > 0) {
+			return true;
+		}
+		return false;
+	}
+}
