cd25f03180f7ce2d6f362d563e49f02ba7d80caa
Maradona Morais <maradona.morais@hotmail.com>
2015-08-03 15:04:12 -0300

    Criação do model de Aluno

---
+++ application/models/Aluno_md.php
@@ -0,0 +1,12 @@
+<?php
+class Aluno_md extends CI_Model {
+	public $nome;
+	public $sobrenome;
+	public $data_nascimento;
+	public $email;
+	
+	public function cadastrarAluno() {
+		//Função de cadastro
+	}
+}
+?>
