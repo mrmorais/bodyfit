dda712f347f932ba5a894a6d626bf561b48c7dda
unknown <20121084010027@AP209839.ifrn.local>
2015-08-04 15:02:34 -0300

    Criação do Model de Academia

---
+++ application/models/Academia_md.php
@@ -0,0 +1,17 @@
+﻿<?php
+class Academia_md extends CI_Model {
+	public $nome;
+	public $cidade;
+	public $estado;
+	public $endereco;
+	public $telefone;
+	
+	public function cadastrarAcademia($nome, $cidade, $estado, $endereco, $telefone) {
+		$this->load->database();
+		$dados = array("nome"=>$nome, "cidade"=>$cidade, "estado"=>$estado, "endereco"=>$endereco, "telefone"=>$telefone);
+		$this->db->insert('academia', $dados);
+		
+		return $this->db->insert_id();
+	}
+}
+?>
